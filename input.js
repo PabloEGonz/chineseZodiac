98b88eb178ff0eed4bcb35006fbb28a0fef56b6e
Pablo <ezekielgonzalezr@gmail.com>
2023-06-04 17:36:53 -0600

    Apply new functions

--- input.js
+++ input.js
@@ -11,59 +11,56 @@ let rooster = [1933, 1945, 1957, 1969, 1981, 1993, 2005, 2017, 2029];
 let dog = [1934, 1946, 1958, 1970, 1982, 1994, 2006, 2018, 2030];
 let pig = [1935, 1947, 1959, 1971, 1983, 1995, 2007, 2019, 2031];
 
-// import render from "./modules/render.js";
+import render from "./modules/render.js";
 import renderPopup from "./modules/renderPopup.js";
 
 // render(2);
 
 
 let input = document.getElementById("inputyear");
-//event listener to click the submitbutton on enter
 
-
-
-let knowYourZodiac = () => {
+const knowYourZodiac = () => {
     // Takes the input year and stores it in the variable 
     const zodiac = parseInt(input.value);
 
     if (dog.includes(zodiac) === true) {
-
+        renderPopup(0);
     }
     else if (dragon.includes(zodiac) === true) {
-
+        renderPopup(1);
     }
     else if (goat.includes(zodiac) === true) {
-
+        renderPopup(2);
     }
     else if (horse.includes(zodiac) === true) {
-
+        renderPopup(3);
     }
     else if (monkey.includes(zodiac) === true) {
-
+        renderPopup(4);
     }
     else if (ox.includes(zodiac) === true) {
-
+        renderPopup(5);
     }
     else if (pig.includes(zodiac) === true) {
-
+        renderPopup(6);
     }
     else if (rabbit.includes(zodiac) === true) {
-
+        renderPopup(7);
     }
     else if (rat.includes(zodiac) === true) {
-
+        renderPopup(8);
     }
     else if (rooster.includes(zodiac) === true) {
-
+        renderPopup(9);
     }
     else if (snake.includes(zodiac) === true) {
-
+        renderPopup(10);
     }
     else if (tiger.includes(zodiac) === true) {
-
+        renderPopup(11);
     }
     else {
-
+        renderPopup(13);
     }
 }
 
@@ -71,6 +68,7 @@ const button = document.querySelector('#submitbutton');
 
 button.addEventListener("click", knowYourZodiac);
 
+//event listener to click the submitbutton on enter
 input.addEventListener("keyup", (event) => {
     if (event.key === "Enter") {
         button.click()
