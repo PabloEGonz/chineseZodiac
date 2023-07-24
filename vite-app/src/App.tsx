b220c9aeff9d4f2d673ae223b1d570817363fe0f
Pablo <ezekielgonzalezr@gmail.com>
2023-07-23 20:16:34 -0600

    Import home component

--- vite-app/src/App.tsx
+++ vite-app/src/App.tsx
@@ -1,13 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
+import Home from './pages/home'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
+    <Home />
     </>
   )
 }
