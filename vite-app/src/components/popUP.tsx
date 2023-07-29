e8399e3157251bb9738ca6b0350ce82a328ef846
Pablo <ezekielgonzalezr@gmail.com>
2023-07-29 16:32:42 -0600

    Add pop up component

---
+++ vite-app/src/components/popUP.tsx
@@ -0,0 +1,17 @@
+
+const PopUP = () => {
+  return (
+    <div
+    id="modalpopup"
+    className="modal mt-5"
+    aria-labelledby="modal"
+    aria-hidden="true"
+  >
+    <div
+      className="modal-dialog d-flex flex-column align-items-center mt-5 p-5 text-center"
+    ></div>
+  </div>
+  )
+}
+
+export default PopUP
