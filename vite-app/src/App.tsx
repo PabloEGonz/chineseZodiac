caf5f3cf404f9c6a4ffb143b631a301d8ab8eeb9
Pablo <ezekielgonzalezr@gmail.com>
2023-08-05 15:43:28 -0600

    Set up router

--- vite-app/src/App.tsx
+++ vite-app/src/App.tsx
@@ -1,10 +1,23 @@
 import './App.css'
 import Home from './pages/home'
+import Info from './pages/info';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import {createBrowserRouter, RouterProvider, Route, Link } from 'react-router-dom'
+
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />
+    },
+    {
+      path: "/zodiac/:zodiacId",
+      element: <Info />
+    }
+  ])
   return (
     <>
-    <Home />
+    <RouterProvider router={router}/>
     </>
   )
 }
