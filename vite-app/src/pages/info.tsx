068ac34d0cc3eacbbd331845bb750e608e40b539
Pablo <ezekielgonzalezr@gmail.com>
2023-08-05 15:45:00 -0600

    Create info page

---
+++ vite-app/src/pages/info.tsx
@@ -0,0 +1,32 @@
+import { Link, useParams } from "react-router-dom";
+import { getDescription } from "../redux/zodiacSlice";
+import { useAppSelector } from "../redux/hooks";
+import { useAppDispatch } from "../redux/hooks";
+import { useEffect } from "react";
+
+const Info = () => {
+  const dispatch = useAppDispatch();
+  const { description } = useAppSelector((state) => state.zodiac);
+    const { zodiacId } = useParams();
+  useEffect(() => {
+    dispatch(getDescription(zodiacId  || ''));
+  }, []);
+
+  return (
+    <div>
+      `<h1 className="display-3">{description.title}</h1>
+      <h3 className="display-6">{description.intro}</h3>
+      <div className="lead m-5 px-5">{description.description}</div>
+      <div className="pic">
+        <img className="rounded" loading="lazy" src="" />
+      </div>
+      <h3>
+        <Link to="/" id="return">
+          Go Back
+        </Link>
+      </h3>
+    </div>
+  );
+};
+
+export default Info;
